9a0a71b1d283cb9c25d006a3d3b499f6159136d1
witschge <ivan.djurdjevic16@gmail.com>
2024-02-15 11:54:28 +0100

    boilerplates

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      YouTube Single Page
+      <div
+        style={{
+          marginTop: "20px",
+          textAlign: "center",
+          position: "fixed",
+          bottom: "0",
+          width: "100%",
+        }}
+      >
+        Made with ❤️ by witschge
+      </div>
     </div>
   );
 }
-
 export default App;
